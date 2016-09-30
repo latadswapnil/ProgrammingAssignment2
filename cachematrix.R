1a41080a9fb99f0a8a2fb88fb249a22a8602fd6c
latadswapnil <latadswapnil@gmail.com>
2016-09-30 18:58:30 -0500

    Functions has been created by Swapnil

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two function will help to calculate the inverse of the matrix and next time if 
+## functions gets the same matrix to calculate the inverse it will return the previously calculated inverse value.
 
-## Write a short comment describing this function
+## This function will hold matrix for calculating the inverse and further helps to get inverse and modify the matrix to calculate new inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates inverse of the matrix and if this function is already been called for the 
+## same element then function returns previously calculated inverse. That is, it stores inverse value 
+## in global environment. 
 
 cacheSolve <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
         ## Return a matrix that is the inverse of 'x'
 }
